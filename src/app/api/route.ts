384ad9d03f759526e4aaa7a23d8057ee0105a726
makeloff1 <makeloff2@gmail.com>
2024-10-09 21:21:22 +0900

    コネクションプールから接続を再利用するように修正

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,15 +1,11 @@
-import { connectToMongoDb } from "@/lib/mongo";
+import { dbPromise } from "@/lib/mongo";
 
 export const dynamic = "force-dynamic"; // defaults to auto
 
 export async function GET() {
-  const client = await connectToMongoDb();
-  const db = client.db("mydatabase");
-
   try {
-    const collection = db.collection("test");
-    const data = await collection.find({}).toArray();
-    await client.close();
+    const db = await dbPromise;
+    const data = await db.collection("test").find({}).toArray();
     return Response.json({ data }, { status: 200 });
   } catch (error: any) {
     return Response.json(
@@ -20,14 +16,10 @@ export async function GET() {
 }
 
 export async function POST(req: Request) {
-  const client = await connectToMongoDb();
-  const db = client.db("mydatabase");
-
   const { name, age, hoge } = await req.json();
   try {
-    const collection = db.collection("test");
-    const result = await collection.insertOne({ name, age, hoge });
-    await client.close();
+    const db = await dbPromise;
+    const result = await db.collection("test2").insertOne({ name, age, hoge });
     return Response.json({ result }, { status: 201 });
   } catch (error: any) {
     return Response.json(
--- src/lib/mongo.ts
+++ src/lib/mongo.ts
@@ -9,24 +9,47 @@ const options = {
   maxConnecting: 2,
 };
 
-let db: Db;
+// mongo dbへの接続を再利用するため、グローバル変数として宣言
+let mongoClient: MongoClient | null = null;
 
-export const connectToMongoDb = async (): Promise<MongoClient> => {
-  let mongoClient;
+export const connectToMongoDb = async (): Promise<Db> => {
   const uri = process.env.MONGODB_URI;
   if (!uri) {
     throw new Error("Need Env MONGODB_URI");
   }
-
-  try {
-    mongoClient = new MongoClient(uri);
-    console.log("Connecting to MongoDB Atlas cluster...");
-    await mongoClient.connect();
-    console.log("Successfully connected to MongoDB Atlas!");
-
-    return mongoClient;
-  } catch (error) {
-    console.error("Connection to MongoDB Atlas failed!", error);
-    process.exit();
+  const databaseName = process.env.MONGODB_DATABASE_NAME;
+  if (!databaseName) {
+    throw new Error("Need Env MONGODB_DATABASE_NAME");
   }
+
+  if (!mongoClient) {
+    try {
+      mongoClient = new MongoClient(uri, options);
+      console.log("Connecting to MongoDB Atlas cluster...");
+      await mongoClient.connect();
+      console.log("Successfully connected to MongoDB Atlas!");
+    } catch (error) {
+      console.error("Connection to MongoDB Atlas failed!", error);
+      process.exit();
+    }
+  }
+  const dbPromise = mongoClient.db(databaseName);
+  console.log("Connected to database");
+
+  // TODO: 最終的にはバリデーションを入れて、変なデータを入れられないようにする
+  // 各コレクションのバリデーション
+  const collectionName = "test2";
+  const collections = await dbPromise
+    .listCollections({ name: collectionName })
+    .toArray();
+  if (collections.length === 0) {
+    await dbPromise.createCollection(collectionName, {
+      validator: { $expr: { $in: ["$kind", ["dog", "cat", "fish"]] } },
+    });
+    console.log(`Collection ${collectionName} created with validation.`);
+  }
+
+  return dbPromise;
 };
+
+export const dbPromise = connectToMongoDb();
